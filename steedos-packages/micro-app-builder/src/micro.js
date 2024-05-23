68e824cae361f5867318ee2e3a50f3c7da5045df
殷亮辉 <yinlianghui@hotoa.com>
2024-05-23 06:59:03 +0000

    amis调整为antd样式，解决ObjectForm窄字段没有生效

--- steedos-packages/micro-app-builder/src/micro.js
+++ steedos-packages/micro-app-builder/src/micro.js
@@ -2,7 +2,7 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2024-05-06 02:26:31
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2024-05-23 05:56:12
+ * @LastEditTime: 2024-05-23 06:56:57
  * @FilePath: /microapps/steedos-packages/micro-app-builder/src/micro.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -327,7 +327,7 @@ module.exports = {
                     }
                   },
                   isCurrentUrl: isCurrentUrl,
-                  theme: 'cxd'
+                  theme: 'antd'
                 }
               );
 
