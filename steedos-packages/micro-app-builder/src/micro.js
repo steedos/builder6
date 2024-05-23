c6d9c1678ed4bec6b513719652c8c3eb532bc860
殷亮辉 <yinlianghui@hotoa.com>
2024-05-23 09:56:40 +0000

    有时进入应用，会报错amisRequire is not defined，或 amisRequire is not defined，要等amis加载完才执行相关代码

--- steedos-packages/micro-app-builder/src/micro.js
+++ steedos-packages/micro-app-builder/src/micro.js
@@ -2,7 +2,7 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2024-05-06 02:26:31
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2024-05-23 09:17:17
+ * @LastEditTime: 2024-05-23 09:54:45
  * @FilePath: /microapps/steedos-packages/micro-app-builder/src/micro.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -106,9 +106,173 @@ module.exports = {
     const registryAssetsComponents = `
       // TODO:这里是不是不应该直接使用amis.render.client.js中的脚本注册资产包中自定义组件，应该单独写个注册脚本
       Promise.all([
-        waitForThing(window, 'loadJs')
+        waitForThing(window, 'loadJs'),
+        waitForThing(window, 'Builder')
       ]).then(() => {
         loadJs(\`\${Builder.settings.rootUrl}/amis-pages/js/amis.render.client.js\`, (script) => {
+        });
+      });
+    `;
+
+    const embedAmis = `
+      // TODO:执行amis.embed相关逻辑，与上面的amis.render.client.js有重复代码，可以合并这两处脚本，待优化
+      Promise.all([
+        waitForThing(window, 'assetsLoaded'),
+        waitForThing(window, 'amis'),
+      ]).then(() => {
+        let amis = amisRequire('amis/embed');
+        const match = amisRequire('path-to-regexp').match;
+
+        // 如果想用 browserHistory 请切换下这处代码, 其他不用变
+        // const history = History.createBrowserHistory();
+        const history = History.createHashHistory();
+
+        const app = ${JSON.stringify(appSchema.data)};
+
+        function normalizeLink(to, location = history.location) {
+          to = to || '';
+
+          if (to && to[0] === '#') {
+            to = location.pathname + location.search + to;
+          } else if (to && to[0] === '?') {
+            to = location.pathname + to;
+          }
+
+          const idx = to.indexOf('?');
+          const idx2 = to.indexOf('#');
+          let pathname = ~idx
+            ? to.substring(0, idx)
+            : ~idx2
+            ? to.substring(0, idx2)
+            : to;
+          let search = ~idx ? to.substring(idx, ~idx2 ? idx2 : undefined) : '';
+          let hash = ~idx2 ? to.substring(idx2) : location.hash;
+
+          if (!pathname) {
+            pathname = location.pathname;
+          } else if (pathname[0] != '/' && !/^https?\\:\\/\\//.test(pathname)) {
+            let relativeBase = location.pathname;
+            const paths = relativeBase.split('/');
+            paths.pop();
+            let m;
+            while ((m = /^\\.\\.?\\//.exec(pathname))) {
+              if (m[0] === '../') {
+                paths.pop();
+              }
+              pathname = pathname.substring(m[0].length);
+            }
+            pathname = paths.concat(pathname).join('/');
+          }
+
+          return pathname + search + hash;
+        }
+
+        function isCurrentUrl(to, ctx) {
+          if (!to) {
+            return false;
+          }
+          const pathname = history.location.pathname;
+          const link = normalizeLink(to, {
+            ...location,
+            pathname,
+            hash: ''
+          });
+
+          if (!~link.indexOf('http') && ~link.indexOf(':')) {
+            let strict = ctx && ctx.strict;
+            return match(link, {
+              decode: decodeURIComponent,
+              strict: typeof strict !== 'undefined' ? strict : true
+            })(pathname);
+          }
+
+          return decodeURI(pathname) === link;
+        }
+
+        let amisInstance = amis.embed(
+          '#root',
+          app,
+          {
+            location: history.location,
+            data: {
+              // 全局数据，是受控的数据
+            },
+            context: {
+              // 全局上下文数据, 非受控的数据，无论哪一层都能获取到，包括弹窗自定义数据映射后都能获取到。
+              // 可以用来放一下全局配置等。比如 API_HOST, 这样页面配置里面可以通过 \${API_HOST} 来获取到。
+              // API_HOST: 'https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com',
+              spaceId: "${spaceId}",
+              appId: "${appId}",
+              context: {
+                tenantId: "${user?.spaceId}",
+                userId: "${user?.userId}",
+                authToken: "${user?.authToken}"}
+              }
+          },
+          {
+            // watchRouteChange: fn => {
+            //   return history.listen(fn);
+            // },
+            updateLocation: (location, replace) => {
+              location = normalizeLink(location);
+              if (location === 'goBack') {
+                return history.goBack();
+              } else if (
+                (!/^https?\\:\\/\\//.test(location) &&
+                  location ===
+                    history.location.pathname + history.location.search) ||
+                location === history.location.href
+              ) {
+                // 目标地址和当前地址一样，不处理，免得重复刷新
+                return;
+              } else if (/^https?\:\\/\\//.test(location) || !history) {
+                return (window.location.href = location);
+              }
+
+              history[replace ? 'replace' : 'push'](location);
+            },
+            jumpTo: (to, action) => {
+              if (to === 'goBack') {
+                return history.goBack();
+              }
+
+              to = normalizeLink(to);
+
+              if (isCurrentUrl(to)) {
+                return;
+              }
+
+              if (action && action.actionType === 'url') {
+                action.blank === false
+                  ? (window.location.href = to)
+                  : window.open(to, '_blank');
+                return;
+              } else if (action && action.blank) {
+                window.open(to, '_blank');
+                return;
+              }
+
+              if (/^https?:\\/\\//.test(to)) {
+                window.location.href = to;
+              } else if (
+                (!/^https?\\:\\/\\//.test(to) &&
+                  to === history.pathname + history.location.search) ||
+                to === history.location.href
+              ) {
+                // do nothing
+              } else {
+                history.push(to);
+              }
+            },
+            isCurrentUrl: isCurrentUrl,
+            theme: 'antd'
+          }
+        );
+
+        history.listen(state => {
+          amisInstance.updateProps({
+            location: state.location || state
+          });
         });
       });
     `;
@@ -189,160 +353,7 @@ module.exports = {
           <div id="root" class="app-wrapper"></div>
           <script>
             (function () {
-              let amis = amisRequire('amis/embed');
-              const match = amisRequire('path-to-regexp').match;
-
-              // 如果想用 browserHistory 请切换下这处代码, 其他不用变
-              // const history = History.createBrowserHistory();
-              const history = History.createHashHistory();
-
-              const app = ${JSON.stringify(appSchema.data)};
-
-              function normalizeLink(to, location = history.location) {
-                to = to || '';
-
-                if (to && to[0] === '#') {
-                  to = location.pathname + location.search + to;
-                } else if (to && to[0] === '?') {
-                  to = location.pathname + to;
-                }
-
-                const idx = to.indexOf('?');
-                const idx2 = to.indexOf('#');
-                let pathname = ~idx
-                  ? to.substring(0, idx)
-                  : ~idx2
-                  ? to.substring(0, idx2)
-                  : to;
-                let search = ~idx ? to.substring(idx, ~idx2 ? idx2 : undefined) : '';
-                let hash = ~idx2 ? to.substring(idx2) : location.hash;
-
-                if (!pathname) {
-                  pathname = location.pathname;
-                } else if (pathname[0] != '/' && !/^https?\\:\\/\\//.test(pathname)) {
-                  let relativeBase = location.pathname;
-                  const paths = relativeBase.split('/');
-                  paths.pop();
-                  let m;
-                  while ((m = /^\\.\\.?\\//.exec(pathname))) {
-                    if (m[0] === '../') {
-                      paths.pop();
-                    }
-                    pathname = pathname.substring(m[0].length);
-                  }
-                  pathname = paths.concat(pathname).join('/');
-                }
-
-                return pathname + search + hash;
-              }
-
-              function isCurrentUrl(to, ctx) {
-                if (!to) {
-                  return false;
-                }
-                const pathname = history.location.pathname;
-                const link = normalizeLink(to, {
-                  ...location,
-                  pathname,
-                  hash: ''
-                });
-
-                if (!~link.indexOf('http') && ~link.indexOf(':')) {
-                  let strict = ctx && ctx.strict;
-                  return match(link, {
-                    decode: decodeURIComponent,
-                    strict: typeof strict !== 'undefined' ? strict : true
-                  })(pathname);
-                }
-
-                return decodeURI(pathname) === link;
-              }
-
-              let amisInstance = amis.embed(
-                '#root',
-                app,
-                {
-                  location: history.location,
-                  data: {
-                    // 全局数据，是受控的数据
-                  },
-                  context: {
-                    // 全局上下文数据, 非受控的数据，无论哪一层都能获取到，包括弹窗自定义数据映射后都能获取到。
-                    // 可以用来放一下全局配置等。比如 API_HOST, 这样页面配置里面可以通过 \${API_HOST} 来获取到。
-                    // API_HOST: 'https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com',
-                    spaceId: "${spaceId}",
-                    appId: "${appId}",
-                    context: {
-                      tenantId: "${user?.spaceId}",
-                      userId: "${user?.userId}",
-                      authToken: "${user?.authToken}"}
-                    }
-                },
-                {
-                  // watchRouteChange: fn => {
-                  //   return history.listen(fn);
-                  // },
-                  updateLocation: (location, replace) => {
-                    location = normalizeLink(location);
-                    if (location === 'goBack') {
-                      return history.goBack();
-                    } else if (
-                      (!/^https?\\:\\/\\//.test(location) &&
-                        location ===
-                          history.location.pathname + history.location.search) ||
-                      location === history.location.href
-                    ) {
-                      // 目标地址和当前地址一样，不处理，免得重复刷新
-                      return;
-                    } else if (/^https?\:\\/\\//.test(location) || !history) {
-                      return (window.location.href = location);
-                    }
-
-                    history[replace ? 'replace' : 'push'](location);
-                  },
-                  jumpTo: (to, action) => {
-                    if (to === 'goBack') {
-                      return history.goBack();
-                    }
-
-                    to = normalizeLink(to);
-
-                    if (isCurrentUrl(to)) {
-                      return;
-                    }
-
-                    if (action && action.actionType === 'url') {
-                      action.blank === false
-                        ? (window.location.href = to)
-                        : window.open(to, '_blank');
-                      return;
-                    } else if (action && action.blank) {
-                      window.open(to, '_blank');
-                      return;
-                    }
-
-                    if (/^https?:\\/\\//.test(to)) {
-                      window.location.href = to;
-                    } else if (
-                      (!/^https?\\:\\/\\//.test(to) &&
-                        to === history.pathname + history.location.search) ||
-                      to === history.location.href
-                    ) {
-                      // do nothing
-                    } else {
-                      history.push(to);
-                    }
-                  },
-                  isCurrentUrl: isCurrentUrl,
-                  theme: 'antd'
-                }
-              );
-
-              history.listen(state => {
-                amisInstance.updateProps({
-                  location: state.location || state
-                });
-              });
+              ${embedAmis}
             })();
           </script>
         </body>
