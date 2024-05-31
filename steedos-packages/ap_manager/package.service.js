5c4ad0282d9d6541177076b008430743d6b49c63
baozhoutao <baozhoutao@hotoa.com>
2024-05-31 15:46:47 +0800

    add package ap_manager

---
+++ steedos-packages/ap_manager/package.service.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: baozhoutao@steedos.com
+ * @Date: 2023-08-04 09:12:17
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2023-08-07 14:00:36
+ * @Description: 
+ */
+const service = require("./dist/index.js");
+module.exports = service;
---
+++ steedos-packages/ap_manager/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+// default WEBPACK_BUNDLE to development
+const WEBPACK_BUNDLE = process.env.WEBPACK_BUNDLE || "development";
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  target: 'node',    
+  externals: [
+    nodeExternals(),
+    nodeExternals({
+      modulesDir: path.resolve(__dirname, '../../node_modules'),
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    nodeEnv: false // 避免业务代码中的process.env.NODE_ENV在编译时就被替换
+  },
+};
