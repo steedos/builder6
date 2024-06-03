03f2f6ff366ac8d2c0574c3ee6b2990e9dff6e23
包周涛 <baozhoutao@hotoa.com>
2024-06-03 11:38:01 +0000

    [chatbots] 修改input边框样式

--- frontend/src/app/(chatbots)/_components/prompt-form.tsx
+++ frontend/src/app/(chatbots)/_components/prompt-form.tsx
@@ -87,7 +87,7 @@ export function PromptForm({
           tabIndex={0}
           onKeyDown={onKeyDown}
           placeholder="Send a message."
-          className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm border-0"
+          className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm border-0 focus:ring-transparent"
           autoFocus
           spellCheck={false}
           autoComplete="off"
