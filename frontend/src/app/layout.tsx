0be08ccc0754006a4fd4cc4aa47c799cc674ed58
殷亮辉 <yinlianghui@hotoa.com>
2024-06-03 06:59:45 +0000

    只要interface路由加载amis和资产包

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2024-06-03 01:05:35
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2024-06-03 04:08:28
+ * @LastEditTime: 2024-06-03 06:54:53
  * @FilePath: /builder6/frontend/src/app/layout.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,8 +11,6 @@ import clsx from 'clsx'
 import '@/styles/tailwind.css'
 import { type Metadata } from 'next'
 import { SessionProvider } from "next-auth/react"
-import '@/default.steedos.config'
-import { getMainHeadCss, getMainHeadJs, getMainBodyJs } from '@/assets'
 
 export const metadata: Metadata = {
   title: {
@@ -28,7 +26,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const user = {};
   return (
     <html
       lang="en"
@@ -38,14 +35,11 @@ export default function RootLayout({
     >
       <head>
         <link rel="icon" type="image/svg" href="/logo.svg"/>
-        {getMainHeadCss()}
-        {getMainHeadJs()}
       </head>
       <body className="flex h-full flex-col min-h-full bg-white antialiased dark:bg-zinc-900">
         <SessionProvider>
           {children}
         </SessionProvider>
-        {getMainBodyJs(user)}
       </body>
     </html>
   )
