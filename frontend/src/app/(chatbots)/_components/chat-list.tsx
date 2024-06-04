76485056570f91826d2592293d94356b03b4acd0
包周涛 <baozhoutao@hotoa.com>
2024-06-04 01:30:30 +0000

    [chatbots]调整消息列表

--- frontend/src/app/(chatbots)/_components/chat-list.tsx
+++ frontend/src/app/(chatbots)/_components/chat-list.tsx
@@ -2,7 +2,7 @@ import { UIState } from '../_lib/chat/actions'
 import { Session } from '../_lib/types'
 import Link from 'next/link'
 import { UserMessage } from '../_components/stocks/message'
-
+import { ExclamationTriangleIcon } from '@radix-ui/react-icons'
 import { Separator } from '../_components/ui/separator'
 
 export interface ChatList {
@@ -20,12 +20,12 @@ export function ChatList({ messages, session, isShared }: ChatList) {
   console.log(`messages`, messages)
 
   return (
-    <div className="relative mx-auto max-w-2xl px-4">chat list
+    <div className="relative mx-auto max-w-2xl px-4">
       {!isShared && !session ? (
         <>
           <div className="group relative mb-4 flex items-start md:-ml-12">
             <div className="bg-background flex size-[25px] shrink-0 select-none items-center justify-center rounded-md border shadow-sm">
-              ExclamationTriangleIcon //TODO
+            <ExclamationTriangleIcon />
             </div>
             <div className="ml-4 flex-1 space-y-2 overflow-hidden px-1">
               <p className="text-muted-foreground leading-normal">
