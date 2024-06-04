d1ad48498ad01b7fe38b07afc8e96242fb10a865
殷亮辉 <yinlianghui@hotoa.com>
2024-06-04 07:11:39 +0000

    左侧导航显示为project设置的菜单

--- frontend/src/app/(interfaces)/_lib/data.ts
+++ frontend/src/app/(interfaces)/_lib/data.ts
@@ -2,7 +2,7 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2024-06-03 10:03:32
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2024-06-04 05:10:15
+ * @LastEditTime: 2024-06-04 05:38:26
  * @FilePath: /builder6/frontend/src/app/(interfaces)/interfaces/lib/data.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,13 +10,7 @@ const axios = require('axios');
 
 export async function getPageSchema(id: string) {
     try {
-        const url = `${process.env.STEEDOS_ROOT_URL}/api/v1/b6_pages/${id}`;
-        // TODO:换成aws请求
-        const result = await axios.get(url, {
-            "headers": {
-                "Authorization": `Bearer 66593dfaf7b089064654f128,6b009696cc7b2b6c02f7f262f3de2d0028c4ccefa95fc19be880b06dd91f36511903886ffc52511eba7bfb`
-            }
-        });
+        const result = await getRecord("b6_pages", id);
         if (result?.data?.data?.amis_schema) {
             return JSON.parse(result.data.data.amis_schema)
         }
@@ -25,6 +19,22 @@ export async function getPageSchema(id: string) {
         }
     } catch (error) {
         console.log("===getPageSchema====error=====", error);
+        return {}
+    }
+}
+
+export async function getRecord(objectName: string, id: string) {
+    try {
+        const url = `${process.env.STEEDOS_ROOT_URL}/api/v1/${objectName}/${id}`;
+        // TODO:换成aws请求
+        const result = await axios.get(url, {
+            "headers": {
+                "Authorization": `Bearer 66593dfaf7b089064654f128,6b009696cc7b2b6c02f7f262f3de2d0028c4ccefa95fc19be880b06dd91f36511903886ffc52511eba7bfb`
+            }
+        });
+        return result;
+    } catch (error) {
+        console.log("===getRecord====error=====", error);
         return {}
     }
 }
---
+++ frontend/src/app/(interfaces)/_lib/tabs.tsx
@@ -0,0 +1,77 @@
+/*
+ * @Author: 殷亮辉 yinlianghui@hotoa.com
+ * @Date: 2024-06-03 10:03:32
+ * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
+ * @LastEditTime: 2024-06-04 07:09:04
+ * @FilePath: /builder6/frontend/src/app/(interfaces)/interfaces/lib/data.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { getRecord } from "./data";
+import {
+    SidebarSection,
+    SidebarItem,
+    SidebarLabel
+} from '@/components/sidebar';
+import {
+    TicketIcon,
+} from '@heroicons/react/20/solid'
+
+export async function getInterface(projectId: string) {
+    try {
+        const result = await getRecord("b6_projects", projectId);
+        if (result?.data?.data) {
+            return result.data.data
+        }
+        else {
+            return {}
+        }
+    } catch (error) {
+        return {}
+    }
+}
+
+const convertTabItemForSidebarItem = async function(tab: any){
+    if (tab.type === "url") {
+        // 显示为name，链接到url，不用处理
+        return tab;
+    } else if (tab.type === "page") {
+        // 显示为对应的page的name，链接到page的url
+        try {
+            const result = await getRecord("b6_pages", tab.page);
+            const page = result?.data?.data;
+            if (page) {
+                return {
+                    ...tab,
+                    name: page.name,
+                    url:   `/interfaces/${page.project_id}/${page._id}`
+                }
+            }
+            else {
+                return tab;
+            }
+        } catch (error) {
+            return tab;
+        }
+    }
+}
+
+
+export async function getSidebarItemsSection(project: any) {
+    const tabs: any = project.tabs;
+    const tabsConverted: any = [];
+    for(var i = 0;i < tabs.length;i++){
+        tabsConverted.push(await convertTabItemForSidebarItem(tabs[i]));
+    }
+    return (
+        <SidebarSection>
+            {
+                tabsConverted.map((tab: any, tabIndex: number) => (
+                    <SidebarItem href={tab.url} key={tabIndex} target={tab.is_new_window ? "_blank": ""}>
+                        <TicketIcon />
+                        <SidebarLabel>{tab.name}</SidebarLabel>
+                    </SidebarItem>
+                ))
+            }
+        </SidebarSection>
+    );
+}
