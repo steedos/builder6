f6326127363d76a992b67d9fde4e442c4884fc70
陈志培 <chenzhipei@hotoa.com>
2024-06-05 07:00:56 +0000

    build docker

---
+++ docker/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export BUILDER6_VERSION=0.0.1
+echo "#########################################################################"
+echo "builder6 version: ${BUILDER6_VERSION}"
+echo "#########################################################################"
+
+sudo docker-compose build --no-cache \
+    --build-arg ARCH=amd64 \
+    --build-arg NODE_VERSION=14 \
+    --build-arg OS=alpine3.12 \
+    --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")"
+
+docker tag steedos/builder6:latest steedos/builder6:${BUILDER6_VERSION}
