c56618f4a903154f10953213b187f383acd89786
baozhoutao <baozhoutao@hotoa.com>
2024-06-06 17:10:24 +0800

    remove frontend

--- frontend/src/components/site/Logo.tsx
+++
@@ -1,8 +0,0 @@
-export function Logo(props: React.ComponentPropsWithoutRef<any>) {
-  return (
-    <div className="group flex flex-row justify-center items-center">
-      <img className={`${props.className}`} src="/logo.svg"/>
-      <span className="font-bold text-xl pl-2 ">灵犀</span>
-    </div>
-  )
-}
