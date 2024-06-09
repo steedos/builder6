ba6212464dbeb7b5c37e246c50e4c2b291c55d44
殷亮辉 <yinlianghui@hotoa.com>
2024-06-09 12:34:30 +0000

    fix page设计器中保存后tailwind css字段值被清空了

--- steedos-packages/builder6/src/interfaces/designer/save.js
+++ steedos-packages/builder6/src/interfaces/designer/save.js
@@ -2,7 +2,7 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2024-05-06 02:26:31
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2024-06-07 13:06:13
+ * @LastEditTime: 2024-06-09 12:32:27
  * @FilePath: /builder6/steedos-packages/builder6/src/interfaces.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -29,6 +29,7 @@ module.exports = {
     const userSession = user || {};
 
     return await this.getObject('b6_pages').update(pageId, {
+      type: "amis",//触发器中判断了type类型，这里必须传过去
       amis_schema: schema
     }, userSession);
   }
