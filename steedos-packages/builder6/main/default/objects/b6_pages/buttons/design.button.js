b80794dc9db99624ad3fae085227f78c6b4536ef
庄建国 <zhuangjianguo@steedos.com>
2024-06-09 07:11:19 +0000

    整理字段

--- steedos-packages/builder6/main/default/objects/b6_pages/buttons/design.button.js
+++ steedos-packages/builder6/main/default/objects/b6_pages/buttons/design.button.js
@@ -1,5 +1,7 @@
 module.exports = {
-    designVisible: function (object_name, record_id, record_permissions) {
+
+    designVisible: function(object_name, record_id, record_permissions) {
         return Creator.baseObject.actions.standard_edit.visible.apply(this, arguments);
     }
+
 }
