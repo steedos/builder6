1ae623701f4986b436f83e432d9439bd5a3d1722
殷亮辉 <yinlianghui@hotoa.com>
2024-06-11 03:20:48 +0000

    只有amis类型page才显示设计器按钮

--- steedos-packages/builder6/main/default/objects/b6_pages/buttons/design.button.js
+++ steedos-packages/builder6/main/default/objects/b6_pages/buttons/design.button.js
@@ -1,6 +1,9 @@
 module.exports = {
 
-    designVisible: function(object_name, record_id, record_permissions) {
+    designVisible: function(object_name, record_id, record_permissions, data) {
+        if(data && data.type !== "amis"){
+            return false;
+        }
         return Creator.baseObject.actions.standard_edit.visible.apply(this, arguments);
     }
 
