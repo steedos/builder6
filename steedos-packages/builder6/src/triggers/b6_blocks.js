f6869dec181e68877d914ead8c0e190717827cd3
庄建国 <zhuangjianguo@steedos.com>
2024-06-12 04:04:50 +0000

    new objects

---
+++ steedos-packages/builder6/src/triggers/b6_blocks.js
@@ -0,0 +1,47 @@
+const uuid = require("uuid");
+
+export const blockBeforeUpdate = {
+    trigger: { 
+        listenTo: 'b6_blocks', 
+        when: ['beforeInsert', 'beforeUpdate']
+    },
+    async handler(ctx) {
+        const {doc, id  = uuid.v4(), isInsert} = ctx.params;
+
+        
+        if (isInsert) doc._id = id;
+
+        try {
+            let content = "";
+            if (doc.type === 'jsx') {content = doc.jsx;}
+            if (doc.type === 'html') {content = doc.html;}
+            if (doc.type === 'amis') {content = JSON.parse(doc.amis_schema);}
+            doc.tailwind = await this.compileTailwind(content, id);
+        } catch (e) { this.broker.logger.error(e)}
+
+        if (doc.type === 'jsx' && doc.jsx) {
+            try {
+                const builder = await this.compileJsx(doc, id);
+                doc.builder = JSON.stringify(builder);
+            } catch (e) { this.broker.logger.error(e)}
+        }
+
+        if (doc.type === 'html' && doc.html) {
+            try {
+                const builder = await this.compileHtml(doc, id);
+                doc.builder = JSON.stringify(builder);
+            } catch (e) { this.broker.logger.error(e)}
+        }
+
+        if (doc.type === 'amis' && doc.amis_schema) {
+            try {
+                const builder = await this.compileAmis(doc, id);
+                doc.builder = JSON.stringify(builder);
+            } catch (e) { this.broker.logger.error(e)}
+        }
+
+        return  {
+            doc
+        }
+    }   
+}
