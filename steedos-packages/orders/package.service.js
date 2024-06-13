da3433fa244d0a8bf42b3c1b449fab31279b9da5
易仕川 <yishichuan@steedos.com>
2024-06-13 07:38:29 +0000

    fix

--- steedos-packages/orders/package.service.js
+++ steedos-packages/orders/package.service.js
@@ -2,9 +2,9 @@
  * @Author: 易仕川 yishichuan@steedos.com
  * @Date: 2024-06-13 06:02:06
  * @LastEditors: 易仕川 yishichuan@steedos.com
- * @LastEditTime: 2024-06-13 06:09:45
+ * @LastEditTime: 2024-06-13 07:30:20
  * @FilePath: /builder6/steedos-packages/shop/package.service.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-const service = require("./src/index.js");
+const service = require("./dist/index.js");
 module.exports = service;
