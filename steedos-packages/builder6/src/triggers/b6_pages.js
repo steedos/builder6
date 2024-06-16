f450c925853d2cb002ca806481ba336faf94e33b
庄建国 <zhuangjianguo@steedos.com>
2024-06-16 15:10:44 +0000

    使用 iframe 显示 chatbot

--- steedos-packages/builder6/src/triggers/b6_pages.js
+++ steedos-packages/builder6/src/triggers/b6_pages.js
@@ -39,6 +39,37 @@ export const pagesBeforeUpdate = {
             } catch (e) { this.broker.logger.error(e)}
         }
 
+        if (doc.type === 'chatbot' && doc.chatbot_id) {
+            try {
+                doc.html = `
+<style>
+.builder-custom-code {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    flex-shrink: 0;
+    min-height:600px;
+    height: 100%;
+    width: 100%;
+}
+.chatbot-iframe {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    flex-shrink: 0;
+    min-height:600px;
+    height: 100%;
+    width: 100%;
+}
+</style>
+<iframe src="https://builder6.com/chatbots/${doc.chatbot_id}"
+        class="chatbot-iframe"></iframe>
+                `
+                const builder = await this.compileHtml(doc, id);
+                doc.builder = JSON.stringify(builder);
+            } catch (e) { this.broker.logger.error(e)}
+        }
+
 
         return  {
             doc
